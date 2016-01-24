89afae0efbd7f7adf1b2fe77c8d5730b0d885fce
lizheming <secretlzm007@gmail.com>
2016-01-24 18:18:07 +0800

    rm compile app

--- app/common/config/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/**
- * config
- */exports.__esModule = true;exports['default'] = 
-{
-  //cluster_on: true //socket.io not support cluster mode
-  //key: value
-};module.exports = exports['default'];
