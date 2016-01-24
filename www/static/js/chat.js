d6f63206c935be5a832137cd76744e5fe1fb9a5a
张楷豪 <zhangkaihao-xy@360.cn>
2016-01-24 20:16:58 +0800

    背景图修改 添加路由跳转

--- www/static/js/chat.js
+++ www/static/js/chat.js
@@ -11,6 +11,27 @@
 		});
 	}
 
+	function getRoomByUrl() {
+		return location.search.match(/room=(\w+)?($|&)/)[1];
+	}
+
+	var name = localStorage.getItem('name');
+	var id = localStorage.getItem('id');
+	if (!name || !id) {
+		window.location.href = '/index/login';
+	}
+
+	var socket = io('http://localhost:8360');
+	var res = socket.emit('adduser', {
+		room: getRoomByUrl(),
+		userId: id,
+		displayName: name
+	});
+
+	socket.on('user:join', function(data) {
+		console.log(data);
+	});
+
 	var sendBtn = query('.btn-send');
 	sendBtn.addEventListener('click', function() {
 		var inputBox = query('.input-box');
